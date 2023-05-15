84e633d7ffb8ee287bf2184b8aba3a73de02cdfc
Kacper Kilianek <01151053@pw.edu.pl>
2023-05-15 17:36:54 +0200

    Test simplifying

--- test.py
+++ test.py
@@ -1,59 +1,13 @@
 from lexer import Lexer
 from parser import Parser
 
-good_string = '''
-//to jest komentarz, np. z nazwą pliku: correct.ppap
-#abc
-	background: url(watch?v=Ct6BUPvE2sM);
-	color: #f00;
-	box-shadow: none;
-	text-shadow: none;
+text_file_good = open("./good.txt", "r")
+good_string = text_file_good.read()
+text_file_good.close()
 
-.def
-	content: "see below";
-
-p > pre
-	border: 1px solid #999;
-	page-break-inside: avoid;
-
-div + p,
-div > p
-	font-weight: bolder
-
-p img
-	max-width: 100% !important;
-
-pine + apple,
-* + * // * jest poprawnym selektorem
-	orphans: 3;
-	widows: 3rem;
-
-a:visited
-	color: white;
-'''
-
-bad_string = '''
-p + + q // kombinator (np. +, >) nie może pojawić się więcej niż raz między selektorami
-	content: "see below";
-	border: 1px solid #999;
-	page-break-inside: avoid;
-
-div+p,
-div>p
-    font-size: smaller; // nie cztery spacje to niepoprawne wcięcie
-		font-family: serif; // ale zbyt duże wcięcie to też problem
-	font-weight: bolder; // powinna być tabulacja, jak tutaj
-	font-style: normal; // ta linia jest dobrze wcięta
-
-pine + apple
-	max-width: 100% !oops; // !important jest jedynym słowem kluczowym z !
-
-foo _bar, // identyfikatory poprawne
-&baz, // identyfikator niepoprawny
-:quax // ten też jest poprawny
-	display: flex // brak średnika jest niedopuszczalny między deklaracjami
-	margin: 1px // ale jest OK na końcu
-'''
+text_file_bad = open("./bad.txt", "r")
+bad_string = text_file_bad.read()
+text_file_bad.close()
 
 
 if __name__ == '__main__':
